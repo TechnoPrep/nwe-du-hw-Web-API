19c5062b93b026e40c65a4f2a7d6f8757d8a3448
nwe <nathan.ehrlich2621@gmail.com>
2021-07-19 12:11:56 -0600

    Added for loop to add li, working on adding click funtionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 let quizEl = $('#quiz');
 let timerEl = $('#timer');
-let timeLeft = 100;
 
-const questions = {q1:
-    {
-        quest1:{}
-    }}
+
+let timeLeft = 100;
+let qIndex = 0;
+
+const questions = [
+    ['Commonly used data types DO NOT include: ','strings','booleans','alerts','numbers'],
+    ['Arrays in JavaScript can be used to store _____.','numbers and strings', 'other arrays','booleans','all of the above'],
+    ['The condition in an if / else statement is enclosed within _____.', 'quotes','curly brackets','parenthesis','square brackets'],
+    ['String values must be enclosed within _____ when being assigned to variables', 'commas','curly brackets','quotes','parenthesis']
+]
+
+const correctAnswers = ['alerts','all of the above','parenthesis','quotes']
+
+let answer = '';
 
 //Start Quiz Function
 
@@ -15,7 +24,7 @@ function startQuiz(){
     let sQH2 = $('<h2>');
     let sQText = $('<p>');
     let sQBtn = $('<button>')
-    
+
     //Add Text to the Elements
     sQH2.text('Coding Quiz Challenge');
     sQText.text('Try to answer the following code-related questions within the time limit Keep in mind that incorrect answer will penalize your scrore/time by then seconds!');
@@ -36,8 +45,8 @@ function startQuiz(){
 
     $(sQBtn).click(function(){
         $('#quiz-start').remove();
-        // nextQuestion();
-        startTimer(false);
+        startTimer();
+        Question();
     });
 
     
@@ -65,6 +74,42 @@ function startTimer(answerResult){
     if(answerResult === false){
         timeLeft -= 10;
     }
+}
+
+function Question(){    
+    let QuestDiv = $('<div>');
+    let QuestH2 = $('<h2>');
+    let QuestUl = $('<ul>');
+    let AnswerBtn = $('<button>');
+
+    let i = qIndex;
+
+    //Add Text to the Elements
+    QuestDiv.attr('id',`question-${i}`);
+    quizEl.append(QuestDiv);
+
+    QuestH2.text(questions[i][0]);
+    QuestDiv.append(QuestH2);
+
+    QuestDiv.append(QuestUl);
+
+    //Create the Answers from the question array
+    for (let index = 1; index < questions[i].length; index++) {
+        AnswerBtn = $('<button>')
+        AnswerBtn.text(questions[i][index]);
+        AnswerBtn.addClass('btn answerbutton');
+        AnswerBtn.attr('data-answer', `a-${index}`);
+        AnswerBtn.attr('value', questions[i][index]);
+        QuestUl.append(AnswerBtn);
+
+    }
+
+    $(AnswerBtn).click(function(event){
+        answer = $(event.target).attr('answerbutton');
+        console.log(answer);
+    });
+
+
 }
 
 
